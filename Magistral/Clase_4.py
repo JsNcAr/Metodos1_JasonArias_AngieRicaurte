b84e1984af5a7dda4d2dd8957d7d9be7b33c20d5
Jason Nicolas Arias <60324149+JsNcAr@users.noreply.github.com>
2023-02-03 16:33:11 +0000

    update

--- Magistral/Clase_4.py
+++ Magistral/Clase_4.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+A = 1
+B = 1
+omega = 2 * np.pi
+N = 100
+t = np.linspace(0, 10, N)
 
-def main():
-    pass
+r = np.array([A * np.cos(omega * t), A * np.sin(omega * t), B * t])
 
-
-if __name__ == "__main__":
-    main()
+print(r[:10])
