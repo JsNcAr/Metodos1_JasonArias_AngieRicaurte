db10d69cddf134beaca51f2f8822b7e16b67b453
Jason Nicolas Arias <60324149+JsNcAr@users.noreply.github.com>
2023-02-03 16:47:54 +0000

    finished 3D graph

--- Magistral/Clase_4.py
+++ Magistral/Clase_4.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 A = 1
-B = 1
-omega = 2 * np.pi
+B = 3
+omega = 2 * np.pi / 10
 N = 100
 t = np.linspace(0, 10, N)
 
@@ -17,5 +17,4 @@ ax.scatter(r[0], r[1], r[2], c='r', marker='o', s=25)
 
 plt.savefig('Figura_4.png', dpi=300)
 
-fig = plt.figure(figsize=(5, 5))
-ax = fig.add_subplot(111, projection='polar')
+plt.show()
